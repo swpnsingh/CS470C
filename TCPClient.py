22713f82686bacc66c6fe64038ad063efc478ea7
swpnsingh <swapna2013ta@gmail.com>
2014-09-29 19:29:47 -0700

    Python homework
    Install and compile the Python programs TCPClient and UDPClient on one
    host and TCPServer and  UDPServer on another host

---
+++ TCPClient.py
@@ -0,0 +1,10 @@
+from socket import *
+serverName = '127.0.0.1'
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName,serverPort))
+sentence = input('Input lowercase sentence:')
+clientSocket.send(sentence.encode('utf-8'))
+modifiedSentence = clientSocket.recv(1024)
+print ('From Server:', modifiedSentence.decode('utf-8'))
+clientSocket.close()
